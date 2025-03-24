153ca42a690e51a192f139872bcf7aacabe7750d
NiliUnger <149992205+NiliUnger@users.noreply.github.com>
2025-03-23 22:20:43 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -8,12 +8,15 @@ setText(quizInputA, "Ellis Island")
 setText(quizInputB, "Angel Island")
 setText(quizInputC, "Southern Border")
 var score = 0
+var streak = 0
 document.addEventListener('click', function() {
     if (quizInputA.checked) {
         score++
+        streak++
         setText(quizLabel, "Correct!")
     } else {
         setText(quizLabel, "Incorrect! Esther entered through Ellis Island.")
+        streak = 0
     }
 })
 //question 2
@@ -24,9 +27,11 @@ setText(quizInputC, "Hester Street")
 document.addEventListener('click', function() {
     if (quizInputC.checked) {
         score++
+        streak++
         setText(quizLabel, "Correct!")
     } else {
         setText(quizLabel, "Incorrect! Hester Street is a center of Jewish life.")
+        streak = 0
     }
 })
 //question 3
@@ -37,8 +42,21 @@ setText(quizInputC, "Fiets")
 document.addEventListener('click', function() {
     if (quizInputB.checked) {
         score++
+        streak++
         setText(quizLabel, "Correct!")
     } else {
+        streak = 0
         setText(quizLabel, "Incorrect! The word 'kvetch' means to complain.")
     }
 })
+//calculating streak multiplier, score, and shekels earned
+var streakMultiplier = 1 + (streak * 0.1);
+shekel = shekel + score * streakMultiplier * 10 
+setText(quizLabel, "Your score is: " + score + "\n" + "Your streak is: " + streak + "\n" + "You earned " + shekel + " shekels!")
+//delete elements
+document.deleteElement('quizLabel')
+document.deleteElement('quizInputA')
+document.deleteElement('quizInputB')
+document.deleteElement('quizInputC')   
+
+
